20cb6a1ff5bb1198b37c839546314ff084bdf461
ASLAM-stack <arifhossainaslam6@gmail.com>
2025-02-06 17:36:40 +0600

    done 2 section

--- script.js
+++ script.js
@@ -48,3 +48,28 @@ headers.forEach(header => {
         }
     });
 });
+// slider
+
+ 
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
+    });
